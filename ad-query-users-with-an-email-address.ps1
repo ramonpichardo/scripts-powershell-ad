462150cb9135973468be571f6e6eee8bb31e58ff
Ramon <ramonpichardo+github@gmail.com>
2018-07-02 14:37:25 -0700

    Add files via upload

---
+++ ad-query-users-with-an-email-address.ps1
@@ -0,0 +1,14 @@
+﻿cls
+
+$List = Get-Content $env:USERPROFILE\Desktop\usernames.txt
+
+$Users = @()
+
+ForEach ($Item in $List)
+{
+$Users += Get-ADUser -Filter {EmailAddress -eq $Item} -Properties *
+}
+
+$Users | Select Name,Title,Department,PhysicalDeliveryOfficeName,EmailAddress,Manager | Export-Csv -NoType $env:USERPROFILE\Desktop\user_listing.csv
+
+Echo Done.

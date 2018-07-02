ab33d504227e003153b9e411e2ad419cde4e56e1
Ramon <ramonpichardo+github@gmail.com>
2018-07-02 14:09:29 -0700

    Add files via upload

---
+++ ad-query-get-user-info-using-smtp-address.ps1
@@ -0,0 +1,12 @@
+﻿$List = Get-Content $env:USERPROFILE\Desktop\smtp_address_list.txt
+
+$Users = @()
+
+ForEach ($Item in $List)
+{
+$Users += Get-ADUser -Filter {EmailAddress -eq $Item} -Properties *
+}
+
+$Users | Select Name,Title,Department,PhysicalDeliveryOfficeName,EmailAddress,Manager | Export-Csv -NoType "$env:USERPROFILE\Desktop\acme_users.csv"
+
+Echo Done.

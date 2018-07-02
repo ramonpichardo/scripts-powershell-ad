e2f3a7e081ca5b55076f90efec47bea7da61bc5e
Ramon <ramonpichardo+github@gmail.com>
2018-07-02 14:26:13 -0700

    Add files via upload

---
+++ dns-resolve-hostnames-to-ip-addresses.ps1
@@ -0,0 +1,7 @@
+﻿$servers = get-content "$env:USERPROFILE\Desktop\hostnames.txt"
+foreach ($server in $servers) {
+  $addresses = [System.Net.Dns]::GetHostAddresses($server)
+  foreach($a in $addresses) {
+    "{0},{1}" -f $server, $a.IPAddressToString >> "$env:USERPROFILE\Desktop\hostnames-and-ip-addresses.txt"
+  }
+} 
---
+++ dns-resolve-ip-addresses-to-hostnames.ps1
@@ -0,0 +1,8 @@
+﻿Get-Content "$env:USERPROFILE\Desktop\ip-addresses.txt" | ForEach-Object{
+$hostname = ([System.Net.Dns]::GetHostByAddress($_)).Hostname
+if($? -eq $True) {
+  $_ +": "+ $hostname >> "$env:USERPROFILE\Desktop\machinenames.txt"
+}
+else {
+   $_ +": Cannot resolve hostname" >> "$env:USERPROFILE\Desktop\machinenames.txt"
+}}

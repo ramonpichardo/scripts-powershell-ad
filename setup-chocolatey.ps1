f5d4a2029b10987abd0e8ea5fcc4b335f1f56e02
Ramon <ramonpichardo+github@gmail.com>
2018-07-02 02:00:29 -0700

    Add files via upload

---
+++ setup-chocolatey.ps1
@@ -0,0 +1,24 @@
+﻿Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+Write-Host $profile
+(
+echo # Chocolatey profile
+echo $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+echo if (Test-Path($ChocolateyProfile)) {
+echo   Import-Module "$ChocolateyProfile"
+echo }
+)>"$profile"
+
+# Install Chocolatey Core Extension
+choco install -y chocolatey-core.extension
+
+#Install browsers
+choco install -y firefox googlechrome
+
+#Install browser add-ins
+choco install -y adobeshockwaveplayer flashplayerplugin flashplayerppapi jre8 silverlight 
+
+#Install utilities 
+choco install -y 7zip adobereader-update notepadplusplus pstools putty.install visioviewer vlc windirstat winscp
+
+#Install network tools
+choco install -y winpcap wireshark
---
+++ setup-vmware-powercli.ps1
@@ -0,0 +1,2 @@
+﻿# Run [script] as Administrator 
+Install-Module -Name VMware.PowerCLI -RequiredVersion 6.5.4.7155375 -AllowClobber

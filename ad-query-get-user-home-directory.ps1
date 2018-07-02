cd4f30dbdf57edffde2d10b2c4db4e51cc7a7776
Ramon <ramonpichardo+github@gmail.com>
2018-07-02 14:07:38 -0700

    Add files via upload

---
+++ ad-query-get-user-home-directory.ps1
@@ -0,0 +1,12 @@
+﻿<#
+.SYNOPSIS
+
+.DESCRIPTION
+
+.NOTES
+
+#>
+
+cls
+
+Get-Content -Path "$env:USERPROFILE\Desktop\user-homedir-userlist.txt" | Get-ADUser -properties samaccountname, homedrive, homedirectory | ft Name, SamAccountName, HomeDrive, HomeDirectory | Tee-Object -file "$env:USERPROFILE\Desktop\home-directories.txt"

7e67e3a43eeee6619c569e3edc60cca4d5c4e992
Ramon <ramonpichardo+github@gmail.com>
2018-07-02 00:04:46 -0700

    Add script to connect to Exchange Server 2010

---
+++ connect-to-exchange-server-2010.ps1
@@ -0,0 +1,3 @@
+﻿$UserCredential = Get-Credential
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://<FQDN>/PowerShell/ -Authentication Kerberos -Credential $UserCredential
+Import-PSSession $Session

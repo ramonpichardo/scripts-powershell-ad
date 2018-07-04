afcf55093d39889d4830c1f1957e51255f706cac
Ramon Pichardo <ramonpichardo+github@gmail.com>
2018-07-03 23:16:54 -0700

    Move to repo scripts-powershell-exchange

--- exchange-server-2010-management-shell.ps1
+++
@@ -1,3 +0,0 @@
-﻿$UserCredential = Get-Credential
-$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://<FQDN>/PowerShell/ -Authentication Kerberos -Credential $UserCredential
-Import-PSSession $Session

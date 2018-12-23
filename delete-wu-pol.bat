cac783bbd1f8d82f7fd2928b412abab494cce011
Ramon <ramonpichardo+github@gmail.com>
2018-12-23 00:42:06 -0800

    Create delete-wu-pol.bat

---
+++ delete-wu-pol.bat
@@ -0,0 +1,12 @@
+# Log on as a local Administrator
+# Open Administrator console (PowerShell or Command Prompt)
+
+reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\WindowsUpdate" /f
+reg delete "HKCU\Software\Microsoft\WindowsSelfHost" /f
+reg delete "HKLM\Software\Microsoft\Windows\CurrentVersion\WindowsStore\WindowsUpdate" /f
+reg delete "HKLM\Software\Microsoft\Windows\CurrentVersion\WindowsUpdate" /f
+reg delete "HKLM\Software\Microsoft\WindowsSelfHost" /f
+reg delete "HKLM\Software\Policies\Microsoft\Windows\WindowsUpdate" /f
+reg delete "HKLM\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\WindowsStore\WindowsUpdate" /f
+
+shutdown /r /t 10

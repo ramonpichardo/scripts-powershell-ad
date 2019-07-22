039a47be5e1f9da7929386b8c4d40c1705e9d17b
Ramon <ramonpichardo+github@gmail.com>
2019-07-22 11:45:41 -0700

    Update delete-aged-directories-and-files.ps1

--- delete-aged-directories-and-files.ps1
+++ delete-aged-directories-and-files.ps1
@@ -1,2 +1,2 @@
-﻿New-PSDrive -Name "X" -PSProvider FileSystem -Root "\\hlfnp02\common"
+﻿New-PSDrive -Name "X" -PSProvider FileSystem -Root "\\fileserver04\common"
 Get-ChildItem –Path  X: –Recurse | Where-Object LastWriteTime –lt (Get-Date).AddDays(-8) | Remove-Item -Confirm:$false -WhatIf 

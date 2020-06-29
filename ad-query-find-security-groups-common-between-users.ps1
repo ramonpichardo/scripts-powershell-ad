b8d56add6925255aedabd773a2f8733e6e2c6fd9
Ramon <ramonpichardo+github@gmail.com>
2020-06-29 16:48:07 -0700

    Sort output by the Name property

--- ad-query-find-security-groups-common-between-users.ps1
+++ ad-query-find-security-groups-common-between-users.ps1
@@ -19,6 +19,4 @@ ForEach ($Index in (1..($Users.Count - 1)))
 {   $Result = Compare-Object -ReferenceObject $Result -DifferenceObject (Get-ADUser $Users[$Index] -Properties MemberOf | Select -ExpandProperty MemberOf) -IncludeEqual | Where SideIndicator -EQ "==" | Select -ExpandProperty InputObject
 }
 
-$Result | Select @{Label="Name";Expression={Get-ADGroup $_ | Select -ExpandProperty Name }},@{Label="DN";Expression={$_}}
- 
-
+$Result | Select @{Label="Name";Expression={Get-ADGroup $_ | Select -ExpandProperty Name }},@{Label="DN";Expression={$_}} | Sort -Property Name 

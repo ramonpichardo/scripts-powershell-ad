a3ac4550c49fade71ae7977dfacc4e6aeafaf98a
Ramon <ramonpichardo+github@gmail.com>
2021-03-04 15:31:28 -0800

    Comment out dsquery string

--- list-stale-computer-accounts.ps1
+++ list-stale-computer-accounts.ps1
@@ -1,1 +1,2 @@
-dsquery computer "OU=Workstations,OU=Computers,OU=US,DC=amer,DC=acme,DC=com" -inactive 4 -limit 0 | find /c /v ""
+# dsquery computer "OU=Workstations,OU=Computers,OU=US,DC=amer,DC=acme,DC=com" -inactive 4 -limit 0 | find /c /v ""
+

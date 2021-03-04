d540bb2b27beb59df31993c422b7db48c841d238
Ramon <ramonpichardo+github@gmail.com>
2021-03-04 15:37:10 -0800

    Query AD for computers last seen 90 days ago

--- list-stale-computer-accounts.ps1
+++ list-stale-computer-accounts.ps1
@@ -1,2 +1,5 @@
 # dsquery computer "OU=Workstations,OU=Computers,OU=US,DC=amer,DC=acme,DC=com" -inactive 4 -limit 0 | find /c /v ""
 
+$daysSinceLastSeen = 90
+$dateTime = (Get-Date).AddDays(-($daysSinceLastSeen))
+Get-ADComputer -Filter {lastLogonTimestamp -lt $dateTime} -ResultPageSize 10000 -ResultSetSize $Null -Properties Name, OperatingSystem, OperatingSystemVersion, DistinguishedName | Sort -Property Name | Format-Table Name, OperatingSystem, OperatingSystemVersion, DistinguishedName -AutoSize
